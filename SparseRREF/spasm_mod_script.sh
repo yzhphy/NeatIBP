973715c9ac432e48e69061e0f8e98a126c0fa31b
RWUSTC <59015023+RWUSTC@users.noreply.github.com>
2023-01-26 23:20:11 +0800

    Update spasm_mod_script.sh

--- SparseRREF/spasm_mod_script.sh
+++ SparseRREF/spasm_mod_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-git clone https://github.com/cbouilla/spasm.git
-cp modules.c spasm/bench/modules.c
-cp Makefile.am spasm/bench/Makefile.am
-cd spasm
+cd SpaSM_for_NeatIBP
 autoreconf -i
 automake --add-missing
 automake
