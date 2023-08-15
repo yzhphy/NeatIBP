a09374322b0930e9e9a89d66ee258668aa320d4b
Hefeng Xu <rwinters@mail.ustc.edu.cn>
2023-08-15 08:56:06 +0800

    Update spasm_mod_script.sh

--- SparseRREF/spasm_mod_script.sh
+++ SparseRREF/spasm_mod_script.sh
@@ -3,6 +3,7 @@ git clone https://github.com/cbouilla/spasm.git
 cp modules.c spasm/bench/modules.c
 cp Makefile.am spasm/bench/Makefile.am
 cd spasm
+apt-get install libtool
 autoreconf -i
 automake --add-missing
 automake
